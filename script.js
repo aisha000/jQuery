8948d846f19ac8d1e87e7921236fefbb4bb24c65
aisha <68122062+aisha000@users.noreply.github.com>
2024-01-05 19:43:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$("document").ready(function(){
+
+// $("#div").click(function(){
+//    const x = $("#p").html()
+//    $("#sapn1").text(x);
+// })
+
+   $("#div").click(function(){
+      alert("")
+   })
+
+})
